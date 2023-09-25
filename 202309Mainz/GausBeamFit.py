484186669b7f9f0cec3d340c2b68be0693f77b8a
Kotaro-Nishi <kotaro.nishi@nex.phys.s.u-tokyo.ac.jp>
2023-09-25 23:38:42 +0900

    gaus beam fit , output is also works

--- 202309Mainz/GausBeamFit.py
+++ 202309Mainz/GausBeamFit.py
@@ -30,29 +30,14 @@ x = np.arange(len(img))
 y = np.arange(len(img[0]))
 xx,yy  = np.meshgrid(x,y) 
 
-"""
-prm = (1000,100turn on0,1000,500,500)
-img2 = f(prm,xx,yy)
-plt.imshow(img2)
-plt.show()
-"""
-
-
-
 init_val = (10000,1000,1000,1000,1000)
 prm, cov, info, msg, ier = leastsq(chi,init_val,args=(xx,yy,img),full_output=True)
-
 chi2 = np.sum(((f(prm,xx,yy)-img))**2)
 
 for i in range(5):
     print("p{:} : {:10.5f} +- {:10.5f}".format(i,prm[i],np.sqrt(cov[i,i])))
 
-"""
-plt.imshow(img)
-plt.contour(f(prm,xx,yy))
-plt.show()
-"""
-
+fit = f(prm,xx,yy)
 
 
 # create
@@ -60,30 +45,70 @@ pdfname = path[:-4] + ".pdf"
 pdf = PdfPages(pdfname)
 fig = plt.figure(figsize=(8.27,11.69), dpi=600)
 
-plt.subplot(2,2,2)
-plt.imshow(img)
-plt.contour(f(prm,xx,yy))
+plt.rcParams['font.size'] = 14
 
-ax1 = fig.add_subplot(2,2,3,projection="3d")
-surf = ax1.plot_surface(xx, yy, f(prm,xx,yy), cmap='bwr', linewidth=0)
+px_tick = np.linspace(0,2304,6)
+px_ticklabel = np.arange(0,16,3)
 
-"""
-ax2 = fig.add_subplot(4,2,2,projection="3d")
-x_flat = xx.flatten()
-y_flat = yy.flatten()
-img_flat = img.flatten()
-dx = np.ones_like(x_flat)
-dy = np.ones_like(y_flat)
-ax2.bar3d(x_flat,y_flat,0,1,1,img_flat)
-"""
-plt.subplot(2,2,1)
+plt.subplot2grid((3,2),(0,0),colspan=2)
 plt.axis('off')
-
-label = ["Amplitude","x0","y0","xsigma","ysigma"]
-table = [label,prm,np.sqrt(np.diag(cov))]
+label = ["Amplitude",r"$x_0$ [mm]",r"$y_0$ [mm]",r"$\sigma_x$ [mm]",r"$\sigma_y$ [mm]"]
+prm_format = ["{:.5f}".format(item*15/2304) for item in prm]
+cov_format = ["{:.5f}".format(np.sqrt(item)) for item in np.diag(cov)]
+table = [label,prm_format,cov_format]
 table = np.array(table).T
-col_width = [0.2,0.4,0.4]
-plt.table(cellText=table, colLabels= ("name","prm","error"),loc ='center',cellLoc ="center",colWidths=col_width)
+col_width = [0.3,0.35,0.35]
+tab = plt.table(cellText=table, colLabels= ("name","prm","error"),loc ='center',cellLoc ="right",colWidths=col_width)
+tab.auto_set_font_size(False)
+tab.set_fontsize(16)
+tab.scale(0.8,1.6)
 
+
+plt.subplot(3,2,3)
+plt.title("2D plot")
+im = plt.imshow(img)
+plt.contour(fit)
+plt.xticks(px_tick,px_ticklabel) 
+plt.yticks(px_tick,px_ticklabel) 
+plt.colorbar(im)
+plt.xlabel(r"$p_x$ [mm]")
+plt.ylabel(r"$p_y$ [mm]")
+
+
+plt.subplot(3,2,4)
+plt.title(r"holizontal plot : $p_x$ = {:.3f}".format(prm[2]/2304*15))
+plt.plot(img[:][int(prm[2])],label = "data",linewidth=0.1)
+plt.plot(fit[:][int(prm[2])],label = "fit",linewidth =0.3)
+plt.legend()
+plt.xlabel(r"$p_y$ [mm]")
+plt.xticks(px_tick,px_ticklabel) 
+
+
+plt.subplot(3,2,5)
+plt.title(r"vertical plot : $p_y$ = {:.3f}".format(prm[1]/2304*15))
+plt.plot(img[int(prm[1])],label = "data",linewidth=0.1)
+plt.plot(fit[int(prm[1])],label = "fit",linewidth =0.3)
+plt.legend()
+plt.xlabel(r"$p_x$ [mm]")
+plt.xticks(px_tick,px_ticklabel) 
+
+
+ax2 = fig.add_subplot(3,2,6,projection="3d")
+ax2.plot_surface(xx, yy, img, cmap='bwr', linewidth=0)
+plt.xticks(px_tick,px_ticklabel) 
+plt.yticks(px_tick,px_ticklabel) 
+ax2.contour3D(xx,yy,fit)
+ax2.set_zlim(0,prm[0])
+plt.xticks(px_tick,px_ticklabel) 
+plt.yticks(px_tick,px_ticklabel) 
+plt.xlabel(r"$p_x$ [mm]")
+plt.ylabel(r"$p_y$ [mm]")
+
+
+plt.subplots_adjust(wspace =0.4 ,hspace = 0.4)
 pdf.savefig()
 pdf.close()
+
+fit = fit.astype(np.uint16)
+tif_path = path[:-4]+"_FIT.TIF"
+cv2.imwrite(tif_path,fit)
